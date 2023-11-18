7bab90ee2e0855e6b53f87d8feb579789f924291
Rodrigo <velez59@gmail.com>
2023-11-18 11:19:01 -0800

    feat: added getChampionData function and passed data to reccomendation route

--- controllers/recommendation.js
+++ controllers/recommendation.js
@@ -21,8 +21,6 @@ router.post('/recommend', async (req, res) => {  // Marked as async
         const championLowercase = champion.toLowerCase();
         console.log(championLowercase);
 
-        // get user data
-
         // create recommendation in database
         const newRecommendation = await createRecommendation(id, prompt, champion);
         console.log(newRecommendation);
@@ -40,10 +38,15 @@ router.post('/recommend', async (req, res) => {  // Marked as async
   }
 });
 
-router.get('/:champion', (req, res) => {
-  const champion = req.params.champion
-
-  res.render('recommendation', { champion });
+router.get('/:champion', async (req, res) => {
+  try {
+    const champion = req.params.champion
+    // Get champion data
+    const championData = await getChampionData(champion);
+    res.render('recommendation', { champion, championData });
+  } catch (error) {
+    console.log("the following error ocurred >>>", error);
+  }
 });
 
 async function getChampionRecommendation(prompt) {
@@ -90,6 +93,19 @@ async function createRecommendation(id, prompt, champion) {
     return newRecommendation;
   } catch (error) {
     console.log('new reccommendation was not created b/c of', error);
+  }
+};
+
+async function getChampionData(champion) {
+  try {
+    const championCapitalized = champion.charAt(0).toUpperCase() + champion.slice(1);
+
+    const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/champion/${championCapitalized}.json`);
+    const championData = await response.json();
+    console.log('debugging this >>>', championData.data[championCapitalized]);
+    return championData.data[championCapitalized];
+  } catch (error) {
+    console.log('champion data could not be retrieved because of >>>', error);
   }
 };
 
