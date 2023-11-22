4fd00f17d4a14d54e64985a8c0e564213eb2fa49
Rodrigo <velez59@gmail.com>
2023-11-21 20:33:46 -0800

    chore: fixed 404

--- server.js
+++ server.js
@@ -123,9 +123,9 @@ app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
-/* app.get('/404', (req, res) => {
+app.get('/404', (req, res) => {
   res.send('404 Not Found').render('404');
-}); */
+});
 
 
 const PORT = process.env.PORT || 3000;
